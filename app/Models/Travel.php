05c38e5f18e323ef3d9fa8682e8c094980dc83d4
codewsid <mogyahid.projects@protonmail.com>
2023-07-15 23:05:53 +0800

    Created: Models and Migrations

---
+++ app/Models/Travel.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Travel extends Model
+{
+    use HasFactory, HasSlug;
+
+    protected $table = "travels";
+
+    protected $fillable = [
+        'is_public',
+        'slug',
+        'name',
+        'description',
+        'number_of_days',
+        'number_of_nights'
+    ];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
+    /**
+     * =====================
+     * Relationships.
+     * =====================
+     */
+
+    public function tours(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Tour::class);
+    }
+    public function numberOfNights(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value, $attributes) => $attributes['number_of_days'] - 1
+        );
+    }
+}
